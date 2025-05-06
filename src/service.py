365931190bb9c10487d4761ca2ea248b67c8fd8e
TryExceptFinally <olegempix@gmail.com>
2025-05-06 19:22:18 +0300

    исп. ошибки в запросах

--- src/service.py
+++ src/service.py
@@ -4,7 +4,7 @@ from src.database import DatabaseConnector
 def get_volume_path(db_connector: DatabaseConnector, share_uid: int) -> str:
     with db_connector as db:
         result = db.execute(
-            f"""select share_path from shares where share_uid='{share_uid}'"""
+            f"""select share_path from shares where share_uid={share_uid}"""
         ).fetchone()
         return result[0] if result else None
 
@@ -17,7 +17,7 @@ def get_image_name_by_uid(db_connector: DatabaseConnector, uid: int) -> str | No
         return result[0] if result else None
 
 
-def get_image_uid_by_name(db_connector: DatabaseConnector, name: str) -> str:
+def get_image_uid_by_name(db_connector: DatabaseConnector, name: str) -> int:
     with db_connector as db:
         result = db.execute(
             f"""select image_uid from images where image_name='{name}'"""
@@ -29,6 +29,6 @@ def update_image(db_connector: DatabaseConnector, image_uid: int, share_uid: int
     with db_connector as db:
         db.execute(
             f"""update images 
-            set share_uid='{share_uid}' and image_path='{image_path}' 
+            set share_uid={share_uid}, image_path='{image_path}' 
             where image_uid='{image_uid}'"""
         )
